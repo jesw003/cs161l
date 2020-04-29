f0df72161a7a7c8eafe9a777cfa038a1369a9835
jesw003 <kwang029@ucr.edu>
2020-04-28 21:00:23 -0700

    Add files via upload

---
+++ aluControlUnit.v
@@ -0,0 +1,11 @@
+module aluControlUnit (
+    input wire [1:0] alu_op , 
+    input wire [5:0] instruction_5_0 , 
+    output wire [3:0] alu_out  
+    );
+
+// ------------------------------
+// Insert your solution below
+// ------------------------------ 
+
+endmodule
---
+++ controlUnit.v
@@ -0,0 +1,17 @@
+module controlUnit  (
+    input wire [5:0]  instr_op , 
+    output wire reg_dst      ,   
+    output wire  branch    ,     
+    output wire  mem_read ,  
+    output wire  mem_to_reg  ,
+    output wire [1:0]  alu_op  ,        
+    output wire  mem_write  , 
+    output wire  alu_src     ,    
+    output wire  reg_write  , 
+    );
+
+// ------------------------------
+// Insert your solution below
+// ------------------------------ 
+
+endmodule
